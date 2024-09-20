d0a960e19e17192749ef8f31aedb9aa62b87170c
Eslam <41278977+eslam5464@users.noreply.github.com>
2024-09-20 22:38:32 +0300

    add docstring for convert image in converters

--- lib/utils/converters/image.py
+++ lib/utils/converters/image.py
@@ -4,6 +4,14 @@ from PIL import Image
 
 
 def convert_image_type_pillow(image_path: str, output_format: str) -> str:
+    """
+    Converts an image file to a specified format using the Pillow library.
+    :param image_path: The file path of the input image to be converted.
+    :param output_format: The desired output format for the image. It should be a string representing the file extension.
+    :return: The file path of the newly converted image.
+    :raises FileNotFoundError: If the input image file does not exist.
+    :raises ValueError: If the provided output format is empty or exceeds 10 characters.
+    """
     if not os.path.exists(image_path):
         raise FileNotFoundError("Image does not exist")
 
