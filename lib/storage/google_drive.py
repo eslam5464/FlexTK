71fad690b569ca112ade800c0e77e4ef216f2880
Eslam <41278977+eslam5464@users.noreply.github.com>
2024-10-01 08:02:29 +0300

    update google drive in storage to handle credentials instead of service account

--- lib/storage/google_drive.py
+++ lib/storage/google_drive.py
@@ -3,46 +3,42 @@ import logging
 import mimetypes
 import os
 
-import google.auth
+from google.auth.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import Resource, build  # noqa
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from lib.exceptions import GoogleDriveError
 from lib.schemas.google_drive import (
+    DriveCredentials,
     DriveFile,
     DriveFileUpload,
     DriveFolder,
-    DriveServiceAccount,
 )
 
 logger = logging.getLogger(__name__)
 
 
 def _get_gdrive_credentials(
-    service_account: DriveServiceAccount,
-):
+    drive_credentials: DriveCredentials,
+) -> Credentials:
     """
     Retrieves Google Drive API credentials using a service account.
-    :param service_account: The service account credentials used for authorization.
+    :param drive_credentials: The GoogleDrive account credentials used for authorization.
     :return: The credentials object that can be used to authenticate API requests.
     """
-    creds, _ = google.auth.load_credentials_from_dict(
-        info={
-            "type": "service_account",
-            "project_id": service_account.project_id,
-            "private_key_id": service_account.private_key_id,
-            "private_key": service_account.private_key.replace("\\n", "\n"),
-            "client_email": service_account.client_email,
-            "client_id": service_account.client_id,
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": service_account.client_x509_cert_url,
-            "universe_domain": "googleapis.com",
-        },
-    )
+    scopes = drive_credentials.scopes
 
-    return creds
+    if scopes is None:
+        scopes = ["https://www.googleapis.com/auth/drive"]
+
+    flow = InstalledAppFlow.from_client_config(
+        client_config=drive_credentials.model_dump(exclude={"scopes"}),
+        scopes=scopes,
+    )
+    credentials = flow.run_local_server(port=0)
+
+    return credentials
 
 
 def _set_permissions(
@@ -82,20 +78,40 @@ def _set_permissions(
         service.permissions().create(fileId=file_id, body=viewer_permission, fields="id").execute()  # noqa
 
 
+def _get_service(
+    drive_credentials: DriveCredentials,
+):
+    """
+    Retrieves the Google Drive service using the provided service account credentials.
+    :param drive_credentials: The GoogleDrive account credentials used for authorization.
+    :return: Google Drive service object.
+    :raises: Exception if the service creation fails.
+    """
+    credentials = _get_gdrive_credentials(drive_credentials)
+    service = build(serviceName="drive", version="v3", credentials=credentials, cache_discovery=False)
+
+    return service
+
+
 def check_folder_exists(
+    drive_credentials: DriveCredentials,
     folder_name: str,
-    service_account: DriveServiceAccount,
+    is_trashed: bool = False,
 ) -> list[DriveFolder]:
     """
     Checks if a folder with the specified name exists in Google Drive.
+    :param drive_credentials: The GoogleDrive credentials used for authorization.
     :param folder_name: The name of the folder to check.
-    :param service_account: The service account credentials used for authorization.
+    :param is_trashed: Whether to check for folders in the trash.
     :return: A list of GoogleDriveFolder schemas representing matching folders.
     :raises GoogleDriveError: If an error occurs during the folder check process.
     """
     try:
-        service = build(serviceName="drive", version="v3", credentials=_get_gdrive_credentials(service_account))
-        query = f"name = '{folder_name}' and mimeType = 'application/vnd.google-apps.folder' "
+        service = _get_service(drive_credentials=drive_credentials)
+        trashed = "true" if is_trashed else "false"
+        query = (
+            f"name contains '{folder_name}' and mimeType = 'application/vnd.google-apps.folder' and trashed = {trashed}"
+        )
         results = service.files().list(q=query, spaces="drive", fields="files(id, name, parents)").execute()
         items = results.get("files", [])
         drive_folders: list[DriveFolder] = []
@@ -123,20 +139,19 @@ def check_folder_exists(
 
 def upload_files_to_folder(
     files_to_upload: list[DriveFileUpload],
-    service_account: DriveServiceAccount,
+    drive_credentials: DriveCredentials,
 ) -> list[DriveFile]:
     """
     Uploads an image file to a specified Google Drive folder. Load
     pre-authorized user credentials from the environment. For more
     information see https://developers.google.com/identity
     :param files_to_upload: List of GoogleDriveFileUpload object that contains the details of the file.
-    :param service_account: The service account credentials used for authorization.
+    :param drive_credentials: The GoogleDrive credentials used for authorization.
     :return: The ID of the uploaded file if successful, otherwise None.
     :raises GoogleDriveError: If an error occurs during the file upload process.
     """
     try:
-        credentials = _get_gdrive_credentials(service_account)
-        service = build(serviceName="drive", version="v3", credentials=credentials)
+        service = _get_service(drive_credentials=drive_credentials)
         uploaded_files: list[DriveFile] = []
 
         for file_entry in files_to_upload:
@@ -166,7 +181,6 @@ def upload_files_to_folder(
             )
 
         return uploaded_files
-
     except HttpError as error:
         logger.error(msg=f"An error occurred: {error}", extra={"exception": error})
         raise GoogleDriveError(error.reason)
@@ -174,20 +188,20 @@ def upload_files_to_folder(
 
 def create_folder(
     folder_name: str,
-    service_account: DriveServiceAccount,
+    drive_credentials: DriveCredentials,
     parent_folder_id: str | None = None,
-) -> str:
+) -> DriveFolder:
     """
     Creates a new folder in Google Drive. For more
     information see https://developers.google.com/identity
     :param folder_name: The name of the folder to be created.
     :param parent_folder_id: The ID of the Google Drive parent folder where the file will be uploaded.
-    :param service_account: The service account credentials used for authorization.
+    :param drive_credentials: The GoogleDrive credentials used for authorization.
     :return: The ID of the newly created folder.
     :raises GoogleDriveError: If an error occurs while creating the folder.
     """
     try:
-        service = build(serviceName="drive", version="v3", credentials=_get_gdrive_credentials(service_account))
+        service = _get_service(drive_credentials)
         file_metadata = {
             "name": folder_name,
             "mimeType": "application/vnd.google-apps.folder",
@@ -197,22 +211,26 @@ def create_folder(
             "mimeType": "application/vnd.google-apps.folder",
             "parents": [parent_folder_id],
         }
-        file = (
+        folder_data = (
             service.files()
             .create(body=file_metadata_with_parent if parent_folder_id else file_metadata, fields="id")
             .execute()
         )
-        file_id = file.get("id")
-        logger.info(f"Created folder {folder_name} in google drive with id {file_id}")
+        folder_id = folder_data.get("id")
+        logger.info(f"Created folder {folder_name} in google drive with id {folder_id}")
 
         _set_permissions(
-            file_id=file_id,
+            file_id=folder_id,
             service=service,
             view=False,
-            write=True,
+            write=False,
         )
 
-        return file_id
+        return DriveFolder(
+            id=folder_id,
+            name=folder_name,
+            parent_ids=[] if not parent_folder_id else [parent_folder_id],
+        )
     except HttpError as error:
         logger.error(msg=f"An error occurred: {error.reason}", extra={"exception": error})
         raise GoogleDriveError(error.reason)
@@ -221,20 +239,19 @@ def create_folder(
 def download_file(
     file_id: str,
     save_path: str,
-    service_account: DriveServiceAccount,
+    drive_credentials: DriveCredentials,
 ):
     """
     Downloads a file from Google Drive and saves it to the specified
     path. For more information see https://developers.google.com/identity
     :param file_id: The ID of the file to download from Google Drive.
     :param save_path: The local file path where the downloaded file will be saved.
-    :param service_account: The service account credentials used for authorization.
+    :param drive_credentials: The GoogleDrive credentials used for authorization.
     :return: The path where the file was saved.
     :raises GoogleDriveError: If an error occurs during the file download process.
     """
     try:
-        service = build(serviceName="drive", version="v3", credentials=_get_gdrive_credentials(service_account))
-
+        service = _get_service(drive_credentials=drive_credentials)
         request = service.files().get_media(fileId=file_id)
         file = io.BytesIO()
         downloader = MediaIoBaseDownload(file, request)
@@ -257,16 +274,16 @@ def download_file(
 
 
 def delete_all_service_account_folders(
-    service_account: DriveServiceAccount,
+    drive_credentials: DriveCredentials,
 ):
     """
     Deletes all folders owned by a specified Google Drive service account.
-    :param service_account: The service account credentials used for authorization.
+    :param drive_credentials: The GoogleDrive credentials used for authorization.
     :return: A list of IDs of the deleted folders if successful, otherwise an empty list.
     :raises GoogleDriveError: If any error occurs during the deletion process.
     """
     try:
-        service = build(serviceName="drive", version="v3", credentials=_get_gdrive_credentials(service_account))
+        service = _get_service(drive_credentials=drive_credentials)
         query = "mimeType = 'application/vnd.google-apps.folder'"
         results = service.files().list(q=query, fields="files(id, name, parents)").execute()
         folders = results.get("files", [])
