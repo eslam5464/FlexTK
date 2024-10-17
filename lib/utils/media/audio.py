23e59c81653f77c3cad8419cde80332fdc0d1ab7
Eslam <41278977+eslam5464@users.noreply.github.com>
2024-10-17 03:03:12 +0300

    add trim and cut audio using ffmpeg

--- lib/utils/media/audio.py
+++ lib/utils/media/audio.py
@@ -1,10 +1,24 @@
+import logging
 import os
 import re
 import subprocess
+from enum import StrEnum
 
 from lib.schemas.media import AudioDetails
-from lib.utils.misc import convert_time_format_to_seconds
+from lib.utils.misc import (
+    convert_seconds_to_time_format,
+    convert_time_format_to_seconds,
+)
 from lib.wrappers.installed_apps import check_ffmpeg
+
+logger = logging.getLogger(__name__)
+
+
+class SupportedAudioFormat(StrEnum):
+    mp3 = ".mp3"
+    ogg = ".ogg"
+    flac = ".flac"
+    wav = ".wav"
 
 
 @check_ffmpeg
@@ -56,3 +70,146 @@ def get_audio_details_ffmpeg(audio_file_path: str) -> AudioDetails:
         frequency=frequency,
         sound_type=sound_type,
     )
+
+
+def _validate_path_and_extensions(
+    audio_input_path: str,
+    audio_output_path: str,
+) -> None:
+    """
+    Validates the existence of input and output paths, as well as their file extensions.
+    :param audio_input_path: Path to the input audio file.
+    :param audio_output_path: Path to the output audio file.
+    :raises FileNotFoundError: If the input audio file does not exist.
+    :raises NotADirectoryError: If the directory for the output path does not exist.
+    :raises ValueError: If the input or output file has an unsupported audio format.
+    """
+    output_path_only, _ = os.path.split(audio_output_path)
+    _, audio_input_extension = os.path.splitext(audio_input_path)
+    _, audio_output_extension = os.path.splitext(audio_output_path)
+    supported_audio_format: list[str] = [e.value for e in SupportedAudioFormat]
+
+    if not os.path.exists(audio_input_path):
+        raise FileExistsError("Input audio does not exist")
+
+    if not os.path.isdir(output_path_only):
+        raise NotADirectoryError("Output path does not exist")
+
+    if audio_input_extension not in supported_audio_format:
+        raise ValueError(
+            f"Audio extension for input is not supported, the supported extensions are {', '.join(supported_audio_format)}",
+        )
+
+    if audio_output_extension not in supported_audio_format:
+        raise ValueError(
+            f"Audio extension for output is not supported, the supported extensions are {', '.join(supported_audio_format)}",
+        )
+
+
+def _run_ffmpeg_command(args: list[str]) -> None:
+    """
+    Executes the ffmpeg command and handles errors.
+    :param args: List of ffmpeg command arguments.
+    :raises RuntimeError: If ffmpeg fails during the operation.
+    """
+    result = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    if result.returncode != 0:
+        logger.error(
+            f"Ffmpeg failed with error code {result.returncode}",
+            extra={
+                "ffmpeg_return_code": result.returncode,
+                "ffmpeg_error": result.stderr,
+                "ffmpeg_output": result.stdout,
+            },
+        )
+        raise RuntimeError(f"Ffmpeg failed with error code: {result.returncode}")
+
+
+def trim_audio_ffmpeg(
+    audio_input_path: str,
+    audio_output_path: str,
+    start_seconds: int,
+    end_seconds: int,
+):
+    """
+    Trims an audio file between the specified start and end time using ffmpeg.
+    :param audio_input_path: Path to the input audio file.
+    :param audio_output_path: Path to the trimmed output audio file.
+    :param start_seconds: Start time in seconds for trimming.
+    :param end_seconds: End time in seconds for trimming.
+    :raises RuntimeError: If ffmpeg fails during the operation.
+    :raises FileNotFoundError: If the input audio file does not exist.
+    :raises NotADirectoryError: If the directory for the output path does not exist.
+    :raises ValueError: If the input or output file has an unsupported audio format.
+    """
+    _validate_path_and_extensions(
+        audio_input_path=audio_input_path,
+        audio_output_path=audio_output_path,
+    )
+    start_time = convert_seconds_to_time_format(start_seconds).split(".")[0]
+    end_time = convert_seconds_to_time_format(end_seconds).split(".")[0]
+
+    args = [
+        "ffmpeg",
+        "-i",
+        audio_input_path,
+        "-ss",
+        f"{start_time}",
+        "-to",
+        f"{end_time}",
+        "-c:v",
+        "copy",
+        "-c:a",
+        "copy",
+        "-threads",
+        "0",
+        audio_output_path,
+        "-y",
+    ]
+
+    _run_ffmpeg_command(args)
+
+
+def cut_audio_ffmpeg(
+    audio_input_path: str,
+    audio_output_path: str,
+    start_seconds: int,
+    duration_seconds: int,
+):
+    """
+    Cuts an audio file from a start time with a given duration using ffmpeg.
+    :param audio_input_path: Path to the input audio file.
+    :param audio_output_path: Path to the output audio file.
+    :param start_seconds: Start time in seconds for the cut.
+    :param duration_seconds: Duration of the audio to extract in seconds.
+    :raises RuntimeError: If ffmpeg fails during the operation.
+    :raises FileNotFoundError: If the input audio file does not exist.
+    :raises NotADirectoryError: If the directory for the output path does not exist.
+    :raises ValueError: If the input or output file has an unsupported audio format.
+    """
+    _validate_path_and_extensions(
+        audio_input_path=audio_input_path,
+        audio_output_path=audio_output_path,
+    )
+    start_time = convert_seconds_to_time_format(start_seconds).split(".")[0]
+    duration_time = convert_seconds_to_time_format(duration_seconds).split(".")[0]
+
+    args = [
+        "ffmpeg",
+        "-i",
+        audio_input_path,
+        "-ss",
+        f"{start_time}",
+        "-t",
+        f"{duration_time}",
+        "-c:v",
+        "copy",
+        "-c:a",
+        "copy",
+        "-threads",
+        "0",
+        audio_output_path,
+        "-y",
+    ]
+    _run_ffmpeg_command(args)
