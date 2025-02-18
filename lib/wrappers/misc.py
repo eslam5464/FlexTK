444bc35618386800e5db95d1c10b1be57d6a1a80
Eslam <41278977+eslam5464@users.noreply.github.com>
2025-02-18 22:17:04 +0200

    update wrapper functions to include memory usage

--- lib/wrappers/misc.py
+++ lib/wrappers/misc.py
@@ -1,5 +1,7 @@
 import functools
 import logging
+import sys
+import tracemalloc
 from time import perf_counter
 from typing import Any, Callable
 
@@ -14,11 +16,30 @@ def benchmark(logger: logging.Logger | None = None) -> Callable[..., Any]:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         @functools.wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
+            tracemalloc.start()
             start_time = perf_counter()
             value = func(*args, **kwargs)
             end_time = perf_counter()
+            current_mem, peak_mem = tracemalloc.get_traced_memory()
+            tracemalloc.stop()
             run_time = end_time - start_time
-            printed_text = f"Execution of {func.__name__} took {run_time:.2f} seconds."
+
+            if sys.platform == "linux" or sys.platform == "linux2":
+                import resource
+
+                mem_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
+            else:
+                mem_usage = None
+
+            printed_text = f"Execution of {func.__name__} took {run_time:.3f} seconds.\n"
+            printed_text += (
+                f"Current Memory Usage: {current_mem / 1024 :.3f} KB or {current_mem / 1024 / 1024 :.3f} MB.\n"
+            )
+            printed_text += f"Peak Memory Usage: {peak_mem / 1024 / 1024:.3f} MB"
+
+            if mem_usage is not None:
+                print(f"\nMax RSS Memory Usage: {mem_usage:.2f} MB")
+
             print(printed_text)
 
             if logger:
